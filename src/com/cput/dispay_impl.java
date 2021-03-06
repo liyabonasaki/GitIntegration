ebb7617e2ff602719f642f05fab8fe11acfd6643
Liyabona Saki <liyabonasaki@gmail.com>
2021-03-06 16:19:58 +0200

    display data implementation

---
+++ src/com/cput/dispay_impl.java
@@ -0,0 +1,11 @@
+package com.cput;
+
+public class dispay_impl implements  display{
+
+    //call the toString method
+    @Override
+    public void showdata() {
+        data d = new data();
+        d.toString();
+    }
+}
